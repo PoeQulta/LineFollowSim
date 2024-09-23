e36642597fe3ef0f8d1a60077419cb901b9e4ead
Pola Qulta <poequlta199@gmail.com>
2024-09-22 23:04:31 -0400

    Added path utils

--- views.js
+++ views.js
@@ -19,9 +19,6 @@ class robotView
             this.model.pose.x = this.sprite.x;
             this.model.pose.y = this.sprite.y;
             this.model.pose.theta = this.sprite.rotation;
-            const sensorViewPos = {x:this.sprite.x+this.sensor.localPose.x-this.sensor.size/2,
-                y:this.sprite.y+this.sensor.localPose.y-this.sensor.size/2
-            }
             this.sensorView = new PIXI.Graphics().rect(0,0,this.sensor.size,this.sensor.size).stroke({ width: 1, color: "yellow" });
             this.sensorView.x = this.sprite.x+this.sensor.localPose.x-this.sensor.size/2
             this.sensorView.y = this.sprite.y+this.sensor.localPose.y-this.sensor.size/2
@@ -33,6 +30,7 @@ class robotView
             app.stage.addChild(this.ICC);
             app.stage.addChild(this.ICCrad);
             app.stage.addChild(this.sensorView);
+            console.log("Asset loaded!!")
         });
     }
         updatePose(deltaTime)
@@ -57,7 +55,15 @@ class robotView
         {
             return this.sensor.getReading(this.model.pose,env);
         }        
-
+        reset(initPose)
+        {
+          this.sprite.x = initPose.x;
+          this.sprite.y = initPose.y;
+          this.model.pose.x = this.sprite.x;
+          this.model.pose.y = this.sprite.y;
+          this.ICC.x = this.sprite.x
+          this.ICC.y = this.sprite.y
+        }
 }
 const arrayRange = (start, stop, step) =>
     Array.from(
@@ -113,18 +119,35 @@ await app.init({
     backgroundColor: 'white',
     width: 800, height: 800 
 });
+
+class EnvView
+{
+  constructor(path)
+  {
+    this.graphics = null
+    this.env = new Environment(path,(env)=>
+      {
+          this.graphics = new PIXI.Graphics().svg(env.svg);
+          app.stage.addChild(this.graphics);
+          robo = new robotView({x:env.paths[0],y:env.paths[1]});
+      }
+    )
+  }
+  reset()
+  {
+    app.stage.removeChild(this.graphics);
+    this.graphics.destroy();
+  }
+}
 const container = document.getElementById("groundContainer");
 container.appendChild(app.canvas);
+var testPath = localStorage.getItem('lineFollowPath');
+if(!testPath)
+ testPath = "M 50 300 C 100 225 150 50 250 150 C 300 200 150 450 350 450 C 350 275 400 300 400 100 C 400 50 500 150 550 50 C 550 50 550 550 600 350 A 50 50 0 1 1 650 550";  
 
-var testPath = "M 50 300 C 100 225 150 50 250 150 C 300 200 150 450 350 450 C 350 275 400 300 400 100 C 400 50 500 150 550 50 C 550 50 550 550 600 350 A 50 50 0 1 1 650 550";  
 var robo = null
-var env = new Environment(testPath,(env)=>
-    {
-        const graphics = new PIXI.Graphics().svg(env.svg);
-        app.stage.addChild(graphics);
-        robo = new robotView({x:env.paths[0],y:env.paths[1]});
-    }
-)
+var envView = new EnvView(testPath);
+var env = envView.env;
 
 
 var err = new chartView('errorChart',100,'Error','#FF0000');
@@ -133,6 +156,9 @@ var deltaErr = new chartView('deltaChart',100,'deltaError',"#0000FF");
 var errPlant = new Plant(50);
 var controller = new PidController(0,0,0,errPlant);
 var V_Base = document.getElementById("V_Base");
+var refresh_btn = document.getElementById("refresh_btn");
+var path_textarea = document.getElementById("path_textarea");
+path_textarea.value = testPath;
 V_Base.onchange = function() {
     robo.baseV = Number(this.value);
   }
@@ -151,6 +177,14 @@ Kd_input.onchange = function() {
     controller.K_d = this.value;
   }
 
+refresh_btn.onclick = function() {
+  robo.reset({x:env.paths[0],y:env.paths[1]});
+  }
+
+  path_textarea.oninput = function () {
+    localStorage.setItem('lineFollowPath', this.value);
+    location.reload();
+  }
 app.ticker.add((ticker) => {
     if(robo !=null)
         {
